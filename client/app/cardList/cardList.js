70ebc7a1ff22ff2115ef6fe1bf9d13096d200255
Nate Nault <nate@nathannault.com>
2017-03-24 06:17:25 +0800

    Add new app filter to handle filtering by resource category

--- client/app/cardList/cardList.js
+++ client/app/cardList/cardList.js
@@ -1,5 +1,16 @@
 angular.module('hackSource.cardList', [])
-.controller('cardListCtrl', function($scope, Data) {
+.controller('cardListCtrl', function($scope, Data, Resources, Filters) {
+
+  var initializeCats = function () {
+    Resources.getAllCats()
+      .then(function (cats) {
+        $scope.categories = cats;
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+  initializeCats();
 
   $scope.sortType = 'createdAt'; // set the default sort type
   $scope.sortReverse = false;  // set the default sort order
@@ -10,10 +21,40 @@ angular.module('hackSource.cardList', [])
     $scope.sortReverse = !$scope.sortReverse;
   };
 
+  $scope.selectedCat = Filters.getCurrentCat();
+
+  // $scope.selectedCat = function() {
+  //   Filters.updateCurrentCat();
+  // };
+  // $scope.selectedCat();
+
 })
 .directive('myCardList', function() {
   return {
     restrict: 'E',
     templateUrl: 'app/cardList/cardList.html'
+  };
+})
+.filter('filterByCat', function () {
+  return function (items, searchCat) {
+    var filtered = [];
+
+    if (!searchCat) {
+      return items;
+    }
+
+    for (var i = 0; i < items.length; i++) {
+      var item = items[i];
+      var itemCats = [];
+
+      item.Categories.forEach(function(cat) {
+        itemCats.push(cat.title);
+      });
+
+      if (itemCats.indexOf(searchCat) !== -1) {
+        filtered.push(item);
+      }
+    }
+    return filtered;
   };
 });
