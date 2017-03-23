a98f9112ae3eebfb7570860d12684cd9fa6ac1a8
Nate Nault <nate@nathannault.com>
2017-03-24 00:11:18 +0800

    Add sortBy functionality to cardList controller

--- client/app/cardList/cardList.js
+++ client/app/cardList/cardList.js
@@ -1,6 +1,15 @@
 angular.module('hackSource.cardList', [])
 .controller('cardListCtrl', function($scope, Data) {
 
+  $scope.sortType = 'createdAt'; // set the default sort type
+  $scope.sortReverse = false;  // set the default sort order
+
+  // Sort functionality
+  $scope.sortBy = function(sortParam) {
+    $scope.sortType = sortParam;
+    $scope.sortReverse = !$scope.sortReverse;
+  };
+
 })
 .directive('myCardList', function() {
   return {
